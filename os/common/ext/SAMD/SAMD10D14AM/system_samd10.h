e25d964b2924a73e5ec021e566da6ffb0f40746f
Theodore Ateba <tf.ateba@gmail.com>
2022-11-07 20:55:09 +0100

    Add SAMD10 system file.

---
+++ os/common/ext/SAMD/SAMD10D14AM/system_samd10.h
@@ -0,0 +1,39 @@
+/*
+    ChibiOS - Copyright (C) 2006..2022 Theodore Ateba
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    SAMD10/system_samd10.h
+ * @brief   CMSIS system device file for the SAMD10D14AM.
+ *
+ * @defgroup CMSIS_SAMD10 SAMD10D14AM System Device File
+ * @ingroup CMSIS_DEVICE
+ * @{
+ */
+
+#ifndef SYSTEM_SAMD10_H
+#define SYSTEM_SAMD10_H
+
+extern uint32_t SystemCoreClock;
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SYSTEM_SAMD10_H */
