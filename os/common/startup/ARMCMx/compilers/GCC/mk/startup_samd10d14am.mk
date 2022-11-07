ef05b804440497ac04fe3bf84af19b610d313d06
Theodore Ateba <tf.ateba@gmail.com>
2022-11-07 20:56:44 +0100

    Add SAMD10  linker script and startup files.

---
+++ os/common/startup/ARMCMx/compilers/GCC/mk/startup_samd10d14am.mk
@@ -0,0 +1,18 @@
+# List of the ChibiOS generic SAMD10D14AM startup and CMSIS files.
+STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c
+          
+STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v6m.S \
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/vectors.S
+
+STARTUPINC = $(CHIBIOS)/os/common/portability/GCC \
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC \
+             $(CHIBIOS)/os/common/startup/ARMCMx/devices/SAMD10D14AM \
+             $(CHIBIOS)/os/common/ext/ARM/CMSIS/Core/Include \
+             $(CHIBIOS)/os/common/ext/SAMD/SAMD10D14AM
+
+STARTUPLD  = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/ld
+
+# Shared variables
+ALLXASMSRC += $(STARTUPASM)
+ALLCSRC    += $(STARTUPSRC)
+ALLINC     += $(STARTUPINC)
